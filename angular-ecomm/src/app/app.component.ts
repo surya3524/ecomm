1e13b3a775d80e9b2a2ae857107985f2e2a7a9b6
Surya <surya7ms@gmail.com>
2025-07-04 03:49:06 -0500

    1. Added Products list

--- angular-ecomm/src/app/app.component.ts
+++ angular-ecomm/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
+import { ProductsListComponent } from './pages/products-list/products-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ HeaderComponent],
+  imports: [ HeaderComponent, ProductsListComponent],
   template: `
     <app-header></app-header>
+    <app-products-list></app-products-list>
   `,
   styles: '',
 })
