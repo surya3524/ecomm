53379f64952dae2ffca95a52ad1c2cfdaf5f88d1
Surya <surya7ms@gmail.com>
2025-07-04 18:07:49 -0500

    Improves cart UI layout and adjusts image size
    Enhances the shopping cart interface by refining the layout for better readability and visual appeal. Updates item image dimensions from 30x30 to 50x50 for improved clarity and adjusts the total cost section for better alignment and styling.
    
    Improves cart UI layout and image clarity
    
    Refines the shopping cart interface for better readability and visual appeal. Updates item image dimensions from 30x30 to 50x50 for enhanced clarity. Adjusts the total cost section for improved alignment and styling.

--- angular-ecomm/src/app/pages/cart/cart.component.ts
+++ angular-ecomm/src/app/pages/cart/cart.component.ts
@@ -6,36 +6,37 @@ import { CartService } from '../../../services/cart.service';
   selector: 'app-cart',
   imports: [],
   template: `
-    <div>
+    <div class="p-6 flex flex-col gap-6">
+      <h2 class="text-2xl">Shopping Cart</h2>
       @for (cart of cartsList(); track cart.id) {
-          <div class="bg-white shadow-md border rounded-xl p-4 flex flex-row gap-4 items-start">
-            <div>
-              <img
-                [src]="cart.image"
-                class="w-[30px] h-[30px] object-contain"
-              />
-            </div>
-            <div class="flex flex-col mt-2">
-              <span class="text-md font-bold">{{ cart.title }}</span>
-              <span class="text-sm"> {{ '$' + cart.price }}</span>
-            </div>
+        <div class="bg-white shadow-md border rounded-xl p-4 flex flex-row gap-4 items-start">
+          <div>
+            <img
+              [src]="cart.image"
+              class="w-[50px] h-[50px] object-contain"
+            />
           </div>
-        }
+          <div class="flex flex-col mt-2">
+            <span class="text-md font-bold">{{ cart.title }}</span>
+            <span class="text-sm"> {{ '$' + cart.price }}</span>
+          </div>
+        </div>
+      }
 
-  <div class = "bg-white text-xl shadow-orange-50 p-3">Total Cost : {{ '$' + totalPrice() }}</div>
-
-  </div>
-
+      <div class="bg-white text-xl shadow-orange-50 p-3 flex justify-between items-center">
+        <span>Total Cost:</span>
+        <span>{{ '$' + totalPrice() }}</span>
+      </div>
+    </div>
   `,
   styles: ``
 })
 export class CartComponent {
 
-  
   cartService = inject(CartService);
   cartsList = this.cartService.cart;
 
-   totalPrice = computed(() => {
+  totalPrice = computed(() => {
     let total = 0;
     for (const cart of this.cartService.cart()) {
       total += cart.price;
