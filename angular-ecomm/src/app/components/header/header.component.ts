d17024ba676b46a35c4c720139b6e63fdf3f672f
Surya <surya7ms@gmail.com>
2025-07-04 02:55:45 -0500

    1. checking changes

--- angular-ecomm/src/app/components/header/header.component.ts
+++ angular-ecomm/src/app/components/header/header.component.ts
@@ -4,7 +4,7 @@ import { Component, signal } from '@angular/core';
   selector: 'app-header',
   imports: [],
   template: `
-    <div class="bg-slate-100 px-3 py-3 shadow-md">
+    <div class="bg-slate-100 px-3 py-3 shadow-md rounded-lg">
       {{title()}}
     </div>
   `,
